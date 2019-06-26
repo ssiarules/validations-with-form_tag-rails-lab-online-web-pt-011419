3a080bf0c7321e91dc5c6ed8987cca0c8c0ac756
Johnson Joseph <jhonsonj88@gmail.com>
2019-06-26 15:50:03 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post!, only: [:show, :edit, :update]
 
 def index
   @posts = Post.all
@@ -38,5 +39,9 @@ end
 
   def post_params
     params.permit(:title, :category, :content)
+  end
+
+  def set_post!
+    @post = Post.find(params[:id])
   end
 end
