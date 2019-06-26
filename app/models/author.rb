d04bcdec187e4473bec26c5e4c8f54dee8ed14f5
Johnson Joseph <jhonsonj88@gmail.com>
2019-06-26 15:35:20 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+  validates :author, presence: false
+  validates :name, presence: false
+  validates :email, uniqueness: false
+  validates :phone_number, length: {is: 10}
+
 end
